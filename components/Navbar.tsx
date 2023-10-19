1aaa6f364eb64931d1fdcb2ff2cb2455ea305db6
gift56 <efeasiughu@gmail.com>
2023-10-19 13:37:30 -0400

    working on layouts

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,13 @@ import Image from "next/image";
 import Link from "next/link";
 
 const Navbar = () => {
+  const navIcons = [
+    {
+      icon: "/assets/icons/search.svg",
+      alt: "search",
+    },
+  ];
+
   return (
     <header className="w-full">
       <nav className="nav">
@@ -12,8 +19,11 @@ const Navbar = () => {
             width={27}
             height={27}
           />
-          <p className="nav-logo">Shop<span className="text-primary">Wise</span></p>
+          <p className="nav-logo">
+            Shop<span className="text-primary">Wise</span>
+          </p>
         </Link>
+        <div className="flex items-center gap-5"></div>
       </nav>
     </header>
   );

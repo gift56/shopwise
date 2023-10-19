fc6c33a87bedef9fc981ee70f6253bfeb6badc3f
gift56 <efeasiughu@gmail.com>
2023-10-19 13:34:18 -0400

    working on layouts

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,14 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Navbar = () => {
   return (
     <header className="w-full">
-      <nav className="nav"></nav>
+      <nav className="nav">
+        <Link href="/" className="flex items-center gap-1">
+          <Image src="/assets/icons/logo.svg" alt="logo" />
+        </Link>
+      </nav>
     </header>
   );
 };

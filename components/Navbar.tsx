54d3ca473caa7c1f08545e47d3b80722db80572a
gift56 <efeasiughu@gmail.com>
2023-10-19 13:35:42 -0400

    working on layouts

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,8 +5,14 @@ const Navbar = () => {
   return (
     <header className="w-full">
       <nav className="nav">
-        <Link href="/" className="flex items-center gap-1">
-          <Image src="/assets/icons/logo.svg" alt="logo" width={27} height={27} />
+        <Link href="/" className="flex items-center gap-1 select-none">
+          <Image
+            src="/assets/icons/logo.svg"
+            alt="logo"
+            width={27}
+            height={27}
+          />
+          <p className="nav-logo">Shop<span className="text-primary">Wise</span></p>
         </Link>
       </nav>
     </header>

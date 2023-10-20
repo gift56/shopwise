50f07bd88883962ef4a3412adf122bbe1cf428d9
gift56 <efeasiughu@gmail.com>
2023-10-19 21:04:15 -0400

    working on carousel

--- components/HeroCarousel.tsx
+++ components/HeroCarousel.tsx
@@ -2,19 +2,39 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 
 const HeroCarousel = () => {
+  const heroImages = [
+    {
+      imgSrc: "/assets/images/hero-1.svg",
+      alt: "smartwatch",
+    },
+    {
+      imgSrc: "/assets/images/hero-2.svg",
+      alt: "bag",
+    },
+    {
+      imgSrc: "/assets/images/hero-3.svg",
+      alt: "lamp",
+    },
+    {
+      imgSrc: "/assets/images/hero-4.svg",
+      alt: "air fryer",
+    },
+    {
+      imgSrc: "/assets/images/hero-5.svg",
+      alt: "chair",
+    },
+  ];
+
   return (
     <div>
       <Carousel>
         <div>
-          <img src="assets/1.jpeg" />
           <p className="legend">Legend 1</p>
         </div>
         <div>
-          <img src="assets/2.jpeg" />
           <p className="legend">Legend 2</p>
         </div>
         <div>
-          <img src="assets/3.jpeg" />
           <p className="legend">Legend 3</p>
         </div>
       </Carousel>

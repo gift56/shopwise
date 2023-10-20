b0f74aac4c1b57c4e12ce1eb45a4f4968b35cebe
gift56 <efeasiughu@gmail.com>
2023-10-19 22:54:01 -0400

    creating first webscriping lib

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -32,6 +32,7 @@ export async function scrapeAmazonProduct(url: string) {
       $("a.size.base.a-color-price"),
       $(".a-button-selected .a-color-base")
     );
+    console.log({ title, currentPrice });
   } catch (error: any) {
     throw new Error(`Failed to scrape product: ${error.message}`);
   }

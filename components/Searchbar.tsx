aab11080c88c246f7c72385e1f7410c20949ef89
gift56 <efeasiughu@gmail.com>
2023-10-19 21:26:35 -0400

    working on scrapping

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,12 +1,15 @@
 "use client";
 
+import { useState } from "react";
+
 const Searchbar = () => {
+  const [searchPrompt, setSearchPrompt] = useState("");
+  
   const handleSubmit = () => {};
   return (
     <form className="flex flex-wrap gap-4 mt-12" onSubmit={handleSubmit}>
       <input
         type="text"
-        name=""
         placeholder="Search Product link"
         className="searchbar-input"
       />

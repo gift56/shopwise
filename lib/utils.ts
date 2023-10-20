e8a1113744147540640e7afd17dc02bd9d399305
gift56 <efeasiughu@gmail.com>
2023-10-19 22:50:19 -0400

    creating first webscriping lib

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,8 @@
 export function extractPrice(...elements: any) {
   for (const element of elements) {
     const priceText = element.text().trim();
-    if(priceText)
+    if (priceText) return priceText.replace(/\D/g, "");
   }
+
+  return "";
 }

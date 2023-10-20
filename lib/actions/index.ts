7ca997198340179f1e9f0f6811c5a913db1f3139
gift56 <efeasiughu@gmail.com>
2023-10-20 16:49:29 -0400

    interacting with database now

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -8,6 +8,7 @@ export async function scrapeAndStoreProduct(productUrl: string) {
     const scrapedProduct = await scrapeAmazonProduct(productUrl);
     if (!scrapedProduct) return;
 
+    let product = scrapedProduct;
     
   } catch (error: any) {
     throw new Error(`Failed to create/update product: ${error.message}`);

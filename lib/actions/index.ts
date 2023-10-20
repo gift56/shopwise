8d0c073b8861ebc1a4aa020c8bb78c48d95eccb0
gift56 <efeasiughu@gmail.com>
2023-10-20 17:15:02 -0400

    interacting with database now

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,7 +1,9 @@
 "use server";
+import { revalidatePath } from "next/cache";
 import Product from "../models/product.model";
 import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
+import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
   if (!productUrl) return;

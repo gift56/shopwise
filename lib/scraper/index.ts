39d7c436c09206382629a190dbd6d1490d050bab
gift56 <efeasiughu@gmail.com>
2023-10-19 22:12:04 -0400

    creating first webscriping lib

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -7,4 +7,5 @@ export async function scrapeAmazonProduct(url: string) {
   const username = String(process.env.BRIGHT_DATA_USERNAME);
   const password = String(process.env.BRIGHT_DATA_PASSWORD);
   const port = 22225;
+  const session_id = (1000000 * Math.random()) | 0;
 }

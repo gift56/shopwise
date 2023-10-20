248486118c4e80863f623f9c7316bc2aa77dff39
gift56 <efeasiughu@gmail.com>
2023-10-20 17:13:28 -0400

    interacting with database now

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -14,6 +14,28 @@ export async function scrapeAndStoreProduct(productUrl: string) {
     let product = scrapedProduct;
     const existingProduct = await Product.findOne({ url: scrapedProduct.url });
 
+    if (existingProduct) {
+      const updatedPriceHistory: any = [
+        ...existingProduct.priceHistory,
+        { price: scrapedProduct.currentPrice },
+      ];
+
+      product = {
+        ...scrapedProduct,
+        priceHistory: updatedPriceHistory,
+        lowestPrice: getLowestPrice(updatedPriceHistory),
+        highestPrice: getHighestPrice(updatedPriceHistory),
+        averagePrice: getAveragePrice(updatedPriceHistory),
+      };
+    }
+
+    const newProduct = await Product.findOneAndUpdate(
+      { url: scrapedProduct.url },
+      product,
+      { upsert: true, new: true }
+    );
+
+    revalidatePath(`/products/${newProduct._id}`);
   } catch (error: any) {
     throw new Error(`Failed to create/update product: ${error.message}`);
   }

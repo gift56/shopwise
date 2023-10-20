8ecb85efbbf11e1e8e7c80cdade14c35b950d0f7
gift56 <efeasiughu@gmail.com>
2023-10-19 20:38:22 -0400

    working on home

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,9 @@ const Home = () => {
       <section className="trending-section">
         <h2 className="section-text">Trending</h2>
         <div className="flex flex-wrap gap-x-8 gap-y-16">
-          
+          {["Apple Iphone 15", "Book", "Sneaker"].map((product) => (
+            <div></div>
+          ))}
         </div>
       </section>
     </>

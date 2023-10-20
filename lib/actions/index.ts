eac7108a9363c26386ccf10ca73333677b069c82
gift56 <efeasiughu@gmail.com>
2023-10-19 21:54:33 -0400

    creating first webscriping lib

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -4,8 +4,8 @@ export async function scrapeAndStoreProduct(productUrl: string) {
   if (!productUrl) return;
 
   try {
-    
-  } catch (error) {
-   throw new Error(`Failed to create/update product: ${error.message}`) 
+    const scrapedProduct = await scrapeAmazonProduct(productUrl);
+  } catch (error: any) {
+    throw new Error(`Failed to create/update product: ${error.message}`);
   }
 }

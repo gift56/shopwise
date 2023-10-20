cf8d381fb14b54b2291fa4cf4f3c7f6ff6d7e590
gift56 <efeasiughu@gmail.com>
2023-10-19 22:59:58 -0400

    creating first webscriping lib

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -31,6 +31,7 @@ export async function scrapeAmazonProduct(url: string) {
       $(".priceToPay span.a-price-whole"),
       $("a.size.base.a-color-price"),
       $(".a-button-selected .a-color-base"),
+      $(".a-price.a-text-price"),
       $(".a-price .a-offscreen")
     );
     console.log({ title, currentPrice });

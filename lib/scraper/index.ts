ffb0169777befc16fe86632efeea7dea05dd8d9b
gift56 <efeasiughu@gmail.com>
2023-10-19 22:10:01 -0400

    creating first webscriping lib

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -2,5 +2,6 @@ export async function scrapeAmazonProduct(url: string) {
   if (!url) return;
 
   // BrightData Proxy configuration
-  const username = String();
+  const username = String(process.env.BRIGHT_DATA_USERNAME);
+  const password = String(process.env.BRIGHT_DATA_PASSWORD);
 }

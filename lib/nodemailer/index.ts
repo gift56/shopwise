99ce7ba423c3a072615e9c7914ff6d04d10c42e4
gift56 <efeasiughu@gmail.com>
2023-10-21 11:13:06 -0400

    working on modal

---
+++ lib/nodemailer/index.ts
@@ -0,0 +1,10 @@
+"use server";
+
+import nodemailer from "nodemailer";
+
+const Notification = {
+  WELCOME: "WELCOME",
+  CHANGE_OF_STOCK: "CHANGE_OF_STOCK",
+  LOWEST_PRICE: "LOWEST_PRICE",
+  THRESHOLD_MET: "THRESHOLD_MET",
+};
